ca993ee3d71402de4d3264730486a90847193ac6
LupeF <46634202+LupeF@users.noreply.github.com>
2025-02-06 00:13:35 -0800

    uplaod starter files and add mobile hamburger menu

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+const hamburgerMenu = document.querySelector('.hamburger-container');
+const overlay = document.querySelector('.overlay');
+const closeBtn = document.querySelector('.close-button');
+
+hamburgerMenu.addEventListener('click', ()=> {
+    overlay.classList.add('show');
+    overlay.classList.remove('hidden');
+
+})
+
+closeBtn.addEventListener('click', () => {
+    overlay.classList.add('hidden');
+    overlay.classList.remove('show');
+})
