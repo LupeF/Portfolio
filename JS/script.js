c94ce00a4766910bc21a78ed37a149e4b41c9f43
LupeF <46634202+LupeF@users.noreply.github.com>
2025-02-15 16:44:49 -0800

    add about page and keyframe styles

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,11 @@
 const hamburgerMenu = document.querySelector('.hamburger-container');
 const overlay = document.querySelector('.overlay');
 const closeBtn = document.querySelector('.close-button');
-
-hamburgerMenu.addEventListener('click', ()=> {
+hamburgerMenu.addEventListener('click', () => {
     overlay.classList.add('show');
-    overlay.classList.remove('hidden');
-
+    overlay.classList.remove('hidden')
 })
-
 closeBtn.addEventListener('click', () => {
     overlay.classList.add('hidden');
     overlay.classList.remove('show');
-})
+})    
