3357b65bf5f91e2f3ccfa7e7c3bdc44cd4be1604
LupeF <46634202+LupeF@users.noreply.github.com>
2025-08-17 14:25:12 -0700

    add background image and opacity styles

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,10 @@
 const hamburgerMenu = document.querySelector('.hamburger-container');
 const overlay = document.querySelector('.overlay');
 const closeBtn = document.querySelector('.close-button');
-const projectMain = document.querySelector('.project-main');
-const cards = document.querySelectorAll('.content-cont');
-const nextBtn = document.getElementById('next');
-const prevBtn = document.getElementById('prev');
-const visibleCards = 3; 
-const cardWidth = cards[0].offsetWidth + 16; // Including gap/margin
-let index = 0;
+// const projectMain = document.querySelector('.project-main');
+
+// const cardWidth = cards[0].offsetWidth + 16; // Including gap/margin
+
 hamburgerMenu.addEventListener('click', () => {
     overlay.classList.add('show');
     overlay.classList.remove('hidden')
@@ -16,17 +13,3 @@ closeBtn.addEventListener('click', () => {
     overlay.classList.add('hidden');
     overlay.classList.remove('show');
 })    
-
-nextBtn.addEventListener('click', () => {
-    if (index < cards.length - visibleCards) {
-        index++;
-        projectMain.style.transform = `translateX(-${index * cardWidth}px)`;
-    }
-});
-
-prevBtn.addEventListener('click', () => {
-    if (index > 0) {
-        index--;
-        projectMain.style.transform = `translateX(-${index * cardWidth}px)`;
-    }
-});
